fd063e7bbaf33a2e7b9f21a916790643220e1151
Zamir Narro <josue.narro.a@gmail.com>
2026-03-02 22:05:22 -0500

    Update monitor.py

--- control/monitor.py
+++ control/monitor.py
@@ -140,6 +140,7 @@ def setup_mqtt():
         client.username_pw_set(settings.MQTT_USER_PUB,
                                settings.MQTT_PASSWORD_PUB)
         client.connect(settings.MQTT_HOST, settings.MQTT_PORT)
+        client.loop_start()
 
     except Exception as e:
         print('Ocurrió un error al conectar con el bróker MQTT:', e)
